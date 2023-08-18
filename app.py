050f6b673c59589e90f6812d60fc285cd602d5ba
jeffHQ <103541640+jeffHQ@users.noreply.github.com>
2023-08-17 22:16:39 -0500

    act2

--- app.py
+++ app.py
@@ -1,17 +1,24 @@
-from flask import Flask
-
-from flask import request
-
- 
+from flask import Flask, request
 
 app = Flask(__name__)
 
- 
-
 @app.route('/')
-
 def hello():
+    return '<h1>Hello, World!</h1>'
 
- 
+@app.route('/sumar')
+def sumar():
+    param1 = int(request.args.get('param1', 0))
+    param2 = int(request.args.get('param2', 0))
+    resultado = param1 + param2
+    return f'La suma de {param1} y {param2} es: {resultado}'
 
-    return '<h1>2, World!</h1>'
+@app.route('/multiplicar')
+def multiplicar():
+    param1 = int(request.args.get('param1', 1))
+    param2 = int(request.args.get('param2', 1))
+    resultado = param1 * param2
+    return f'El producto de {param1} y {param2} es: {resultado}'
+
+if __name__ == '__main__':
+    app.run()
