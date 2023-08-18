0bbc4b8acffe9455917b1fea99b0821b96265baa
jeffHQ <103541640+jeffHQ@users.noreply.github.com>
2023-08-17 21:07:25 -0500

    feature2

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
-print("hello_word_")
+from flask import Flask
+
+from flask import request
+
+ 
+
+app = Flask(__name__)
+
+ 
+
+@app.route('/')
+
+def hello():
+
+ 
+
+    return '<h1>2, World!</h1>'
